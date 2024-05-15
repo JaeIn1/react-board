e33bb9f24a9c8c501c78466dd3ba1cb60f615570
이재인 <ijaein@ijaein-ui-MacBookPro.local>
2024-05-15 15:28:53 +0900

    add : aside 아이콘 추가 및 css적용

--- src/components/BoardAside.js
+++ src/components/BoardAside.js
@@ -1,7 +1,27 @@
 import "./BoardAside.css";
+import MenuBookIcon from "@mui/icons-material/MenuBook";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import BookmarkRemoveIcon from "@mui/icons-material/BookmarkRemove";
 
 const BoardAside = () => {
-  return <div className="board_aside"></div>;
+  return (
+    <div className="board_aside">
+      <div className="icon_div_parent">
+        <div className="icon_div">
+          <MenuBookIcon className="board_icon" />
+          <span>Board</span>
+        </div>
+        <div className="icon_div">
+          <BookmarkRemoveIcon className="board_icon" />
+          <span>Blog</span>
+        </div>
+        <div className="icon_div">
+          <FacebookIcon className="board_icon" />
+          <span>FaceBook</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BoardAside;
